40fa3f2ed24e89e8f5e5f30bcf75e1c115ee1021
Andrew Walz <83365991+heyguyswalzhere@users.noreply.github.com>
2023-04-13 08:34:08 -0500

    Added the files I have so far...
    I'm still working on getting the site set up...

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>walzTube</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="assets/js/commonActions.js"></script>
+
+</head>
+<body>
+    
+    <div id="pageContainer">
+
+
+         <div id="mastHeadContainer">
+         <button  class="navShowHide">
+            <img src="assets/images/icons/sharkMenu.png">
+
+         </button>
+         
+         <a class ="logoContainter" href="index.php">
+         <img src="assets/images/icons/walzCastSiteLogo.png"  title = "walztube logo" style="
+         height:35px;
+         ">
+
+        </div>
+
+        <div id="sideNavContainer" style= "display:none;">
+
+
+
+        </div>
+
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+               Why is this a link? 
+
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
